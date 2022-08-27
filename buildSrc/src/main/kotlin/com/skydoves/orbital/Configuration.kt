3d9301540affe0fab104510d5d34e54e6daf7a60
skydoves <skydoves@naver.com>
2022-08-27 11:28:54 +0900

    Prepare for the next release

--- buildSrc/src/main/kotlin/com/skydoves/orbital/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/orbital/Configuration.kt
@@ -6,9 +6,9 @@ object Configuration {
   const val minSdk = 21
   const val majorVersion = 0
   const val minorVersion = 2
-  const val patchVersion = 1
+  const val patchVersion = 2
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 5
+  const val versionCode = 6
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
